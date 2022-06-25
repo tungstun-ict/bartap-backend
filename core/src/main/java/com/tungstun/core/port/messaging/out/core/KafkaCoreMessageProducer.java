be4e3e13f2a57602b4005a12f173d42d6129f19b
ChromaChroma <jona.beer@gmail.com>
2022-06-25 17:03:54 +0200

    Core messaging producer

---
+++ core/src/main/java/com/tungstun/core/port/messaging/out/core/KafkaCoreMessageProducer.java
@@ -0,0 +1,39 @@
+package com.tungstun.core.port.messaging.out.core;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaCoreMessageProducer {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaCoreMessageProducer.class);
+    private static final String TOPIC = "core";
+
+    private final KafkaTemplate<String, Object> template;
+
+    public KafkaCoreMessageProducer(@Qualifier("coreTemplate") KafkaTemplate<String, Object> template) {
+        this.template = template;
+    }
+
+    public void publish(String id, Object data) {
+        template.send(TOPIC, id, data)
+                .addCallback(
+                        success -> LOG.info("Published message {} with id {}", data, id),
+                        failure -> LOG.warn("Failed to publish message. Id: {}. Data: {}. Cause: {}. ",
+                                id, data, failure.getLocalizedMessage())
+                );
+    }
+
+    public void publish(Number id, Object data) {
+        publish(String.valueOf(id), data);
+    }
+
+    @Bean
+    private NewTopic core() {
+        return new NewTopic(TOPIC, 1, (short) 1);
+    }
+}
