76cf07adbb2395925fb62eb21d847f69dfc61a05
JortWillemsen <jort@maurice-nienke.nl>
2022-06-26 15:25:33 +0200

    Fix gradle and docker compose

--- gateway/src/main/java/com/tungstun/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/tungstun/gateway/GatewayApplication.java
@@ -5,14 +5,31 @@ import com.tungstun.gateway.route.RouteUriConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties(RouteUriConfig.class)
 @EnableWebFluxSecurity
+@EnableSwagger2
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
