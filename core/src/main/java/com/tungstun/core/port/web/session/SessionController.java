eb293390b05db79fbfb07eeb7b11a3968b952f0a
ChromaChroma <jona.beer@gmail.com>
2022-06-26 14:34:54 +0200

    Add Session Controller endpoints Add Controller request and response dto's

--- core/src/main/java/com/tungstun/core/port/web/session/SessionController.java
+++ core/src/main/java/com/tungstun/core/port/web/session/SessionController.java
@@ -2,8 +2,21 @@ package com.tungstun.core.port.web.session;
 
 import com.tungstun.core.application.session.SessionCommandHandler;
 import com.tungstun.core.application.session.SessionQueryHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tungstun.core.application.session.command.*;
+import com.tungstun.core.application.session.query.GetActiveSession;
+import com.tungstun.core.application.session.query.GetSession;
+import com.tungstun.core.application.session.query.ListSessionsOfBar;
+import com.tungstun.core.domain.session.Session;
+import com.tungstun.core.port.web.session.request.CreateSessionRequest;
+import com.tungstun.core.port.web.session.request.GetActiveBarRequest;
+import com.tungstun.core.port.web.session.request.ListSessionsOfBarRequest;
+import com.tungstun.core.port.web.session.request.UpdateSessionRequest;
+import com.tungstun.core.port.web.session.response.SessionIdResponse;
+import com.tungstun.core.port.web.session.response.SessionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bars")
@@ -14,5 +27,62 @@ public class SessionController {
     public SessionController(SessionQueryHandler queryHandler, SessionCommandHandler commandHandler) {
         this.queryHandler = queryHandler;
         this.commandHandler = commandHandler;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public SessionIdResponse createSession(CreateSessionRequest request) {
+        Long sessionId = commandHandler.handle(new CreateSession(request.barId(), request.name()));
+        return new SessionIdResponse(sessionId);
+    }
+
+    @PutMapping("/{sessionId}")
+    @ResponseStatus(HttpStatus.OK)
+    public SessionIdResponse updateSession(
+            @PathVariable("sessionId") Long sessionId,
+            UpdateSessionRequest request) {
+        commandHandler.handle(new UpdateSessionName(sessionId, request.name()));
+        return new SessionIdResponse(sessionId);
+    }
+
+    @DeleteMapping("/{sessionId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteSession(@PathVariable("sessionId") Long sessionId) {
+        commandHandler.handle(new DeleteSession(sessionId));
+    }
+
+    @PatchMapping("/{sessionId}/end")
+    @ResponseStatus(HttpStatus.OK)
+    public void endSession(@PathVariable("sessionId") Long sessionId) {
+        commandHandler.handle(new EndSession(sessionId));
+    }
+
+    @PatchMapping("/{sessionId}/lock")
+    @ResponseStatus(HttpStatus.OK)
+    public void lockSession(@PathVariable("sessionId") Long sessionId) {
+        commandHandler.handle(new LockSession(sessionId));
+    }
+
+    @GetMapping("/active")
+    @ResponseStatus(HttpStatus.OK)
+    public SessionResponse getActiveSession(@RequestBody GetActiveBarRequest request) {
+        Session session = queryHandler.handle(new GetActiveSession(request.barId()));
+        return SessionResponse.of(session);
+    }
+
+    @GetMapping("/{sessionId}")
+    @ResponseStatus(HttpStatus.OK)
+    public SessionResponse getSession(@PathVariable("sessionId") Long sessionId) {
+        Session session = queryHandler.handle(new GetSession(sessionId));
+        return SessionResponse.of(session);
+    }
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<SessionResponse> getAllSession(@RequestBody ListSessionsOfBarRequest request) {
+        List<Session> sessions = queryHandler.handle(new ListSessionsOfBar(request.barId()));
+        return sessions.stream()
+                .map(SessionResponse::of)
+                .toList();
     }
 }
---
+++ core/src/main/java/com/tungstun/core/port/web/session/response/SessionResponse.java
@@ -0,0 +1,24 @@
+package com.tungstun.core.port.web.session.response;
+
+import com.tungstun.core.domain.session.Session;
+
+import java.time.LocalDateTime;
+
+public record SessionResponse(
+        Long id,
+        String name,
+        LocalDateTime creationDate,
+        LocalDateTime endDate,
+        Boolean isLocked,
+        Boolean isEnded) {
+
+    public static SessionResponse of(Session session) {
+        return new SessionResponse(
+                session.getId(),
+                session.getName(),
+                session.getCreationDate(),
+                session.getEndDate(),
+                session.isLocked(),
+                session.isEnded());
+    }
+}
