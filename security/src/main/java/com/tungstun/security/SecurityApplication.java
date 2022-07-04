d7339650ccfe69f7a33d82165797f4e1602bd7a9
JortWillemsen <jort@maurice-nienke.nl>
2022-07-04 16:31:50 +0200

    Fix PR feedback

--- security/src/main/java/com/tungstun/security/SecurityApplication.java
+++ security/src/main/java/com/tungstun/security/SecurityApplication.java
@@ -3,12 +3,9 @@ package com.tungstun.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 
 @SpringBootApplication
 @ComponentScan("com.tungstun")
-@EnableWebMvc
 public class SecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
