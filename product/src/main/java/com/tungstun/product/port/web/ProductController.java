e650d726052a8a3c632523c9e62573741aab5313
JortWillemsen <jort@maurice-nienke.nl>
2022-07-05 14:34:01 +0200

    Fix PR feedback

--- product/src/main/java/com/tungstun/product/port/web/ProductController.java
+++ product/src/main/java/com/tungstun/product/port/web/ProductController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/bills")
+@RequestMapping("/products")
 public class ProductController {
 
     @GetMapping
