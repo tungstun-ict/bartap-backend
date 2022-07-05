4d4ae5d9a8012be47d041e22078689eec0e41ebb
ChromaChroma <jona.beer@gmail.com>
2022-07-05 20:01:22 +0200

    Add BillCreated and BillDeleted handling

---
+++ core/src/main/java/com/tungstun/core/port/messaging/in/bill/KafkaBillMessageConsumer.java
@@ -0,0 +1,40 @@
+package com.tungstun.core.port.messaging.in.bill;
+
+import com.tungstun.core.application.session.SessionCommandHandler;
+import com.tungstun.core.application.session.command.AddBill;
+import com.tungstun.core.application.session.command.RemoveBill;
+import com.tungstun.core.port.messaging.in.bill.message.BillCreated;
+import com.tungstun.core.port.messaging.in.bill.message.BillDeleted;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(id = "billMessageListener", topics = "bill")
+public class KafkaBillMessageConsumer {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaBillMessageConsumer.class);
+    private final SessionCommandHandler commandHandler;
+
+    public KafkaBillMessageConsumer(SessionCommandHandler commandHandler) {
+        this.commandHandler = commandHandler;
+    }
+
+    @KafkaHandler
+    public void handleBillCreated(BillCreated o) {
+        LOG.info("Received BillCreated: {}", o);
+        commandHandler.handle(new AddBill(o.id(), o.billId()));
+    }
+
+    @KafkaHandler
+    public void handleBillCreated(BillDeleted o) {
+        LOG.info("Received BillDeleted: {}", o);
+        commandHandler.handle(new RemoveBill(o.id(), o.billId()));
+    }
+
+    @KafkaHandler(isDefault = true)
+    public void unknown(Object o) {
+        LOG.warn("Received unknown: {}", o);
+    }
+}
---
+++ core/src/main/java/com/tungstun/core/port/messaging/in/bill/message/BillDeleted.java
@@ -0,0 +1,6 @@
+package com.tungstun.core.port.messaging.in.bill.message;
+
+public record BillDeleted(
+        Long id,
+        Long billId) {
+}
