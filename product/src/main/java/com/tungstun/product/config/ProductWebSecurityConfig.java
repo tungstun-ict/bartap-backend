f823c545a91ae2e0bb602b081ac8060e19143718
Jort Willemsen <32990828+JortWillemsen@users.noreply.github.com>
2022-07-05 14:38:45 +0200

    Create rudementary Product service (#28)
    Closes #14
    * Create rudementary Product service

---
+++ product/src/main/java/com/tungstun/product/config/ProductWebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.tungstun.product.config;
+
+import com.tungstun.common.security.filter.JwtAuthenticationFilter;
+import com.tungstun.common.security.jwt.JwtValidator;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@Order(999)
+@Primary
+public class ProductWebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtValidator validator;
+    private final String[] ignoredPaths = {
+            "/api/swagger-ui/**",
+            "/api/v3/api-docs/**",
+            "/api/v3/api-docs/swagger-config"
+    };
+
+    public ProductWebSecurityConfig(JwtValidator validator) {
+        this.validator = validator;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, ignoredPaths).permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .addFilter(new JwtAuthenticationFilter(
+                        this.authenticationManager(),
+                        validator,
+                        ignoredPaths
+                ))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
---
+++ product/src/test/java/com/tungstun/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tungstun.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
