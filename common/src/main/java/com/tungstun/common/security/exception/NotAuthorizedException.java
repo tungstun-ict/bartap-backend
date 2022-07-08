b56b8f697b2e1f258259601dd7c07b01261ccfc0
ChromaChroma <jona.beer@gmail.com>
2022-07-08 15:24:58 +0200

    Remove unused constructor

--- common/src/main/java/com/tungstun/common/security/exception/NotAuthorizedException.java
+++ common/src/main/java/com/tungstun/common/security/exception/NotAuthorizedException.java
@@ -4,12 +4,7 @@ package com.tungstun.common.security.exception;
 import org.springframework.security.access.AccessDeniedException;
 
 public class NotAuthorizedException extends AccessDeniedException {
-
     public NotAuthorizedException(String msg) {
         super(msg);
-    }
-
-    public NotAuthorizedException(String msg, Throwable cause) {
-        super(msg, cause);
     }
 }
