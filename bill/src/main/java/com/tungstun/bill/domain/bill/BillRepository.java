6dd846acf98cfa4a563f1d205a2a9db9861cbc7a
ChromaChroma <jona.beer@gmail.com>
2022-07-09 14:25:27 +0200

    Add Delete Bill (entity) to repositories

--- bill/src/main/java/com/tungstun/bill/domain/bill/BillRepository.java
+++ bill/src/main/java/com/tungstun/bill/domain/bill/BillRepository.java
@@ -4,7 +4,10 @@ import com.tungstun.common.persistence.CrudRepositoryFragment;
 
 import java.util.List;
 
-public interface BillRepository  extends CrudRepositoryFragment<Bill, Long> {
+public interface BillRepository extends CrudRepositoryFragment<Bill, Long> {
     List<Bill> findAllBySessionId(Long id);
+
     List<Bill> findAllByCustomerId(Long id);
+
+    void delete(Bill bill);
 }
