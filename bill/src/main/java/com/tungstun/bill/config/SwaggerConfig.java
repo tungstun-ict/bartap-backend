5ca05c8f6203b00be3e40da539670da400cb9890
ChromaChroma <jona.beer@gmail.com>
2022-07-09 13:17:30 +0200

    Add Swagger Api Definition

---
+++ bill/src/main/java/com/tungstun/bill/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.tungstun.bill.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.models.annotations.OpenAPI31;
+
+@OpenAPI31
+@OpenAPIDefinition(
+        info = @Info(
+                title = "bartap Backend Api - Bill",
+                description = "Bill API of the bartap Backend API microservice cluster containing bill functionality",
+                version = "1.0",
+                contact = @Contact(
+                        name = "Tungstun",
+                        url = "https://github.com/tungstun-ict",
+                        email = "jort@tungstun.nl"
+                )
+        ),
+        tags = {
+                @Tag(name = "Bill", description = "Functionality based around the Bill"),
+        }
+)
+public class SwaggerConfig {
+}
