090ce14dd874014d6da96249d7445118520026bd
ChromaChroma <jona.beer@gmail.com>
2022-07-10 22:21:08 +0200

    Add wrapped function for ProductType.valueOf()

--- product/src/main/java/com/tungstun/product/domain/product/ProductType.java
+++ product/src/main/java/com/tungstun/product/domain/product/ProductType.java
@@ -1,7 +1,17 @@
 package com.tungstun.product.domain.product;
 
+import java.util.Locale;
+
 public enum ProductType {
     DRINK,
     FOOD,
-    OTHER
+    OTHER;
+
+    public static ProductType getProductType(String type) {
+        try {
+            return ProductType.valueOf(type.toUpperCase(Locale.ROOT));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(String.format("'%s' is not an existing product type", type), e);
+        }
+    }
 }
