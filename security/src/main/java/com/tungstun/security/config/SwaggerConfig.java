0c4ce91033ba13a8b3b7f07a8110b0856e1d731a
ChromaChroma <jona.beer@gmail.com>
2022-07-10 20:23:30 +0200

    Add OpenAPI definition

---
+++ security/src/main/java/com/tungstun/security/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.tungstun.security.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.models.annotations.OpenAPI31;
+
+@OpenAPI31
+@OpenAPIDefinition(
+        info = @Info(
+                title = "bartap Backend Api - Security",
+                description = "Security API of the bartap Backend API microservice cluster containing user and authorization functionality",
+                version = "1.0",
+                contact = @Contact(
+                        name = "Tungstun",
+                        url = "https://github.com/tungstun-ict",
+                        email = "jort@tungstun.nl"
+                )
+        ),
+        tags = {
+                @Tag(name = "User", description = "Functionality based around the user"),
+                @Tag(name = "Authentication", description = "Functionality based around the authentication"),
+                @Tag(name = "Authorization", description = "Functionality based around the authorization"),
+        }
+)
+public class SwaggerConfig {
+}
