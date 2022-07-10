62bef399adc432b3d1485e5f04025eabd599cf94
ChromaChroma <jona.beer@gmail.com>
2022-07-11 00:33:34 +0200

    Add common IdResponse

---
+++ common/src/main/java/com/tungstun/common/web/IdResponse.java
@@ -0,0 +1,7 @@
+package com.tungstun.common.web;
+
+/**
+ * Common response Data Transfer Object for Web API responses containing just an entity's ID value
+ */
+public record IdResponse(Long id) {
+}
