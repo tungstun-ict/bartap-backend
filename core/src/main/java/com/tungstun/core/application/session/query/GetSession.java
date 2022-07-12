da882b9a80cbaf1e5985e43adfe0ca07550e8ec5
ChromaChroma <jona.beer@gmail.com>
2022-07-12 16:44:46 +0200

    Add /bars/{barId} path param in front of SessionController

--- core/src/main/java/com/tungstun/core/application/session/query/GetSession.java
+++ core/src/main/java/com/tungstun/core/application/session/query/GetSession.java
@@ -3,7 +3,7 @@ package com.tungstun.core.application.session.query;
 import javax.validation.constraints.NotNull;
 
 public record GetSession(
-        @NotNull(message = "id cannot be empty")
+        @NotNull(message = "Session id cannot be empty")
         Long id,
         @NotNull(message = "Bar id cannot be empty")
         Long barId) {
--- core/src/main/java/com/tungstun/core/port/web/session/request/GetSessionRequest.java
+++
@@ -1,4 +0,0 @@
-package com.tungstun.core.port.web.session.request;
-
-public record GetSessionRequest(Long barId) {
-}
