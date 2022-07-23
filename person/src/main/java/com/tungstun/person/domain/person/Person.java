2bc120e258b89fd88a8f3a5ceba61d411a9700f4
ChromaChroma <jona.beer@gmail.com>
2022-07-23 14:34:01 +0200

    Add Person base domain

---
+++ person/src/main/java/com/tungstun/person/domain/person/Person.java
@@ -0,0 +1,48 @@
+package com.tungstun.person.domain.person;
+
+import com.tungstun.person.domain.user.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "bar_id")
+    private Long barId;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    private User user;
+
+    public Person() {
+    }
+
+    public Person(Long barId, String name, User user) {
+        this.barId = barId;
+        this.name = name;
+        this.user = user;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getBarId() {
+        return barId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
---
+++ person/src/main/java/com/tungstun/person/domain/user/User.java
@@ -0,0 +1,48 @@
+package com.tungstun.person.domain.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "\"user\"")
+public class User {
+    @Id
+    private Long id;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    public User() {
+
+    }
+    public User(Long id, String phoneNumber, String username, String fullName) {
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.username = username;
+        this.fullName = fullName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
