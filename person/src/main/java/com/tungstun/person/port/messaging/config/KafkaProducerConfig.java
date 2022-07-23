288ce28758979ba877b80eed5b15fe5c75d2db49
ChromaChroma <jona.beer@gmail.com>
2022-07-23 17:47:42 +0200

    Add events to kafka producer config serializable classes

--- person/src/main/java/com/tungstun/person/port/messaging/config/KafkaProducerConfig.java
+++ person/src/main/java/com/tungstun/person/port/messaging/config/KafkaProducerConfig.java
@@ -2,6 +2,9 @@ package com.tungstun.person.port.messaging.config;
 
 import com.tungstun.common.messaging.KafkaConfigBase;
 import com.tungstun.common.messaging.KafkaMessageProducer;
+import com.tungstun.person.application.person.event.PersonCreated;
+import com.tungstun.person.application.person.event.PersonDeleted;
+import com.tungstun.person.application.person.event.PersonUpdated;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +14,11 @@ import java.util.Set;
 @Configuration
 public class KafkaProducerConfig extends KafkaConfigBase {
     private static final String TOPIC = "person";
-    private static final Set<Class<?>> CLASSES = Set.of();
+    private static final Set<Class<?>> CLASSES = Set.of(
+            PersonCreated.class,
+            PersonUpdated.class,
+            PersonDeleted.class
+    );
 
     @Bean
     public NewTopic person() {
