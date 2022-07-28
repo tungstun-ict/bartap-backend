27193484664b86cbfc1cf9521da7add31cd71936
ChromaChroma <jona.beer@gmail.com>
2022-07-28 16:36:02 +0200

    Add Eureka to project Add eureka server Add eureka to all client services and properties Fix Person service has product application name

--- bill/src/main/java/com/tungstun/bill/BillApplication.java
+++ bill/src/main/java/com/tungstun/bill/BillApplication.java
@@ -2,12 +2,14 @@ package com.tungstun.bill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @ComponentScan("com.tungstun")
 @EnableKafka
+@EnableEurekaClient
 public class BillApplication {
     public static void main(String[] args) {
         SpringApplication.run(BillApplication.class, args);
--- product/src/main/java/com/tungstun/product/ProductApplication.java
+++ product/src/main/java/com/tungstun/product/ProductApplication.java
@@ -2,10 +2,14 @@ package com.tungstun.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @ComponentScan("com.tungstun")
+@EnableKafka
+@EnableEurekaClient
 public class ProductApplication {
 
     public static void main(String[] args) {
