1eb9cd0b6fcfe6392164d6768aba9bfbc6c48d56
ChromaChroma <jona.beer@gmail.com>
2022-07-28 14:34:58 +0200

    Fix broken test failing

--- bill/src/test/java/com/tungstun/bill/application/bill/BillCommandProducerTest.java
+++ bill/src/test/java/com/tungstun/bill/application/bill/BillCommandProducerTest.java
@@ -6,6 +6,7 @@ import com.tungstun.bill.application.bill.command.UpdateBillPayed;
 import com.tungstun.bill.domain.bill.Bill;
 import com.tungstun.bill.domain.person.Person;
 import com.tungstun.bill.port.persistence.bill.SpringBillRepository;
+import com.tungstun.bill.port.persistence.person.SpringPersonRepository;
 import com.tungstun.common.test.MessageProducerTestBases;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.AfterEach;
@@ -14,7 +15,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.concurrent.TimeUnit;
 
@@ -27,9 +27,9 @@ class BillCommandProducerTest extends MessageProducerTestBases {
     @Autowired
     private BillCommandHandler commandHandler;
     @Autowired
-    private SpringBillRepository repository;
+    private SpringBillRepository billRepository;
     @Autowired
-    private JpaRepository<Person, Long> personRepository;
+    private SpringPersonRepository personRepository;
 
     private Person customer;
     private Bill bill;
@@ -43,13 +43,13 @@ class BillCommandProducerTest extends MessageProducerTestBases {
     protected void setUp() {
         super.setUp();
         customer = personRepository.save(new Person(456L, BAR_ID, "customer"));
-        bill = repository.save(new Bill(BAR_ID, 456L, customer));
+        bill = billRepository.save(new Bill(BAR_ID, 456L, customer));
     }
 
     @AfterEach
     protected void tearDown() {
         super.tearDown();
-        repository.deleteAll();
+        billRepository.deleteAll();
     }
 
     @Test
@@ -65,7 +65,7 @@ class BillCommandProducerTest extends MessageProducerTestBases {
     @Test
     void payBill_PublishesBillPayed() throws InterruptedException {
         Long id = bill.getId();
-        commandHandler.handle(new UpdateBillPayed(id, customer.getId(), true));
+        commandHandler.handle(new UpdateBillPayed(id, bill.getBarId(), true));
 
         ConsumerRecord<String, String> singleRecord = records.poll(100, TimeUnit.MILLISECONDS);
         assertNotNull(singleRecord);
